8650625a1f3b870bc71fe6a2b0e09059c1a75815
Soumodeep Paul <soumodeeppaul@Soumodeeps-MacBook-Pro.local>
2021-02-23 11:25:41 +0530

    hi1

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello all! I have been seen {} times.\n'.format(count)
+    return 'Hello ! I have been seen {} times.\n'.format(count)
